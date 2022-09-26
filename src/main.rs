b17c3b7d21d1f972b9cbb5809c4f4cdf1e67b656
Gabriel Guesser <gdguesser@gmail.com>
2022-09-26 21:55:46 +0100

    Adding necessary dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use aws_config::meta::region::RegionProviderChain;
+use aws_sdk_dynamodb::Client;
+use aws_sdk_dynamodb::model::AttributeValue;
+use lambda_runtime::{handler_fn, Context, Error as LambdaError};
+use serde::Deserialize;
+use serde_json::{json, Value};
+use uuid::Uuid;
+
+#[tokio::main]
+async fn main() -> Result<(), LambdaError> {
+
 }
